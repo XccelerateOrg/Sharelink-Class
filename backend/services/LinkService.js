1739e01b3e05b9fef74148816f64cd9834123b28
Lesley C <lesley.yc@gmail.com>
2021-07-20 17:40:52 +0800

    added more comments to sharelink

--- backend/services/LinkService.js
+++ backend/services/LinkService.js
@@ -5,6 +5,8 @@ class LinkService {
 
   async addLink(link, user_id) {
     console.log("Adding Link");
+
+    // adding title and url into links
     let query = await this.knex
       .insert({
         title: link.title,
@@ -16,6 +18,7 @@ class LinkService {
         throw new Error(err);
       });
 
+    // adding link_id and user_id into links_users
     await this.knex
       .insert({
         link_id: query[0],
@@ -27,6 +30,8 @@ class LinkService {
         throw new Error(err);
       });
 
+    // mapping array of links
+    // ensuring that you return the data
     link.tags.map(async (tag) => {
       let query1 = await this.knex
         .select("*")
@@ -40,7 +45,9 @@ class LinkService {
           throw new Error(err);
         });
 
+      //
       if (query1[0] === undefined) {
+        // insert tag.name into tags
         await this.knex
           .insert({
             name: tag.name,
@@ -60,6 +67,7 @@ class LinkService {
               });
           });
       } else {
+        // insert link_id and tag_id into links_tags
         await this.knex
           .insert({
             link_id: query[0],
@@ -73,6 +81,7 @@ class LinkService {
       }
     });
 
+    // return object with id, user_id as well as link
     return {
       id: query[0],
       ...link,
@@ -82,6 +91,7 @@ class LinkService {
 
   async list(search, id) {
     if (search.length > 0) {
+      // grab the link id, title, url and link_tag's tag_id and tag's name
       let query = await this.knex
         .select(
           "l.id",
@@ -90,31 +100,47 @@ class LinkService {
           "lt.tag_id",
           "t.name"
         )
+        // from the links table
         .from("links as l")
+        // joining the links and links_tags table
         .join("links_tags as lt", "lt.link_id", "l.id")
+        // joining the tags table
         .join("tags as t", "t.id", "lt.tag_id")
+        // joining the links_users table
         .join("links_users as lu", "lu.link_id", "l.id")
+        // joining the users table
         .join("users as u", "u.id", "lu.user_id")
+        // where the user id matches the passed in id
         .where("u.id", id)
+        // and where the specific condition is
         .andWhere((subcondition) =>
           subcondition
+            // where the link title contains the search parameter
             .where("l.title", "ilike", `%${search}%`)
-            // .orWhere("l.url", "ilike", `%${search}%`)
+            // or where the tag name contains the search parameter
             .orWhere("t.name", "ilike", `%${search}%`)
         );
 
+      // create an array called link array
       let linkArray = [];
       console.log(query);
       let prevLink;
 
+      // loop through the array called query
       for (let i = 0; i < query.length; i++) {
         if (prevLink === undefined) {
+          // create a tag query variable
           let tagQuery = await this.knex
+            // grabbing the name
             .select("t.name")
+            // from the table tags
             .from("tags as t")
+            // joining the links_tags table
             .join("links_tags as lt", "lt.tag_id", "t.id")
+            // where the link tag id matches the query's id
             .where("lt.link_id", `${query[i].id}`);
 
+          // push into the link array, the query as well as the array of tags
           linkArray.push({
             ...query[i],
             tags: tagQuery,
@@ -143,30 +169,47 @@ class LinkService {
     } else {
       console.log("no search");
 
+      // join links_users, grab link_id, id
+      // join from users where user id is the id passed in
       let linkQuery = await this.knex
+        // grab the id, title, url and user_id
         .select("l.id", "l.title", "l.url", "lu.user_id")
+        // from the table links
         .from("links as l")
+        // join the links and links_users table
         .join("links_users as lu", "lu.link_id", "l.id")
+        // join the users table
         .join("users as u", "u.id", "lu.user_id")
+        // grab the specific instance that matches with the id passed in
         .where("u.id", id);
 
       let newLinks = [];
 
+      // loop through the array of links
       for (let i = 0; i < linkQuery.length; i++) {
         let queryTags = await this.knex
+          // grab the tag name
           .select("t.name")
+          // from table tags
           .from("tags as t")
+          // join tag_id and tag id
           .join("links_tags as lt", "lt.tag_id", "t.id")
+          // where the link tag's link_id is equivalent to the id that we loop over
           .where("lt.link_id", `${linkQuery[i].id}`);
 
+        // create an object called link
         let link = {
+          // put the link object in
           ...linkQuery[i],
+          // as well as the array called tags
           tags: queryTags,
         };
 
+        // push the link into the newLinks array
         newLinks.push(link);
       }
 
+      // return the array of objects
       return newLinks;
     }
   }
